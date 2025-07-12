f2d8985c5a7a7431b37490f75863bfc2132776cc
mamun <morzmamun@gmail.com>
2025-07-12 21:41:51 +0600

    fix: adjust background opacity

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -16,7 +16,7 @@ export const LandingPage = () => {
         >
           <source src="/rain-drop.mp4" type="video/mp4" />
         </video>
-        <div className="absolute inset-0 bg-black opacity-85" />
+        <div className="absolute inset-0 bg-black opacity-50" />
       </div>
 
       {/* Content */}
